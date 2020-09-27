066253f32d958788e0efc8fd685279fa9d25f70f
sugartechllc <sugartechllc@gmail.com>
2020-09-27 16:50:35 +0000

    Working code for accessing the ccs811/bme280 combo device.

--- README.md
+++ README.md
@@ -3,17 +3,51 @@ Air quality to CHORDS
 
 ## Setup
 Enable I2C using raspi-config
+
+        sudo raspi-config
+
+Enable I2C clock stretching by setting the I2C baud rate in `/boot/config.txt`:
+
+        dtparam=i2c_arm_baudrate=10000
+
+Install i2c tools:
+
+        sudo apt-get install i2c-tools
+
+Install chip support:
+
+        # Install the sparkfun qwiic python support
+        sudo pip3 install sparkfun-qwiic
+
+## Testing
+Verify that both chips are there:
 ```
-sudo raspi-config
+sudo i2cdetect -y 1
+sudo: unable to resolve host airq
+     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+00:          -- -- -- -- -- -- -- -- -- -- -- -- -- 
+10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+50: -- -- -- -- -- -- -- -- -- -- -- 5b -- -- -- -- 
+60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+70: -- -- -- -- -- -- -- 77
 ```
 
-```
-pip3 install adafruit-circuitpython-bme280
-```
+Verify the BME280:
 
+        python3 bme280.py
+
+Verify the CCS811:
+
+        python ccs811.py
 
 ## Resources
 
+- Sparkfun python docs for the [BME280](https://qwiic-bme280-py.readthedocs.io/en/latest/?)
+- Sparkfun python docs for the [CCS811](https://qwiic-ccs811-py.readthedocs.io/en/latest/?)
 - Sparkfun [CCS811 hookup guide](https://learn.sparkfun.com/tutorials/ccs811bme280-qwiic-environmental-combo-breakout-hookup-guide?_ga=2.42719461.1539937089.1601160436-1748549399.1600881830).
 - Sparkfun [GitHub CCS811 repository](https://github.com/sparkfun/CCS811_Air_Quality_Breakout).
 - Sparkfun [Qwicc-shim](https://learn.sparkfun.com/tutorials/qwiic-shim-for-raspberry-pi-hookup-guide?_ga=2.122920139.1539937089.1601160436-1748549399.1600881830).
+- Note about the chip [not being supported on rPi](https://raspberrypi.stackexchange.com/questions/74418/pi-cannot-communicate-with-i2c-sensor) :-(.
---
+++ ccs811_bme280.py
@@ -0,0 +1,94 @@
+import qwiic_ccs811
+import qwiic_bme280
+import time
+"""
+CCS811
+
+# pylint: disable=C0103
+# pylint: disable=C0325
+"""
+"""
+Sparkfun CCS881/BME280 Combo Board
+
+Note: Sparkfun admits that the BME280 measurements will be
+inaccurate due to its proximity to the hot CCS811 chip.
+This class can be used with standalone CCS811 and BME280 
+chips, just by specifying the I2C adddresses for them.
+
+Code for this class was borrowed from the examples at
+https://qwiic-ccs811-py.readthedocs.io/en/latest/?
+"""
+class ccs811_bme280(object):
+  _deviceErrors = { \
+    1 << 5 : "HeaterSupply",  \
+      1 << 4 : "HeaterFault", \
+      1 << 3 : "MaxResistance", \
+      1 << 2 : "MeasModeInvalid",  \
+      1 << 1 : "ReadRegInvalid", \
+      1 << 0 : "MsgInvalid" \
+  }
+
+  def __init__(self, ccs811_address=0x5b, bme280_address=0x77):
+    self.ccs811 = qwiic_ccs811.QwiicCcs811(address=ccs811_address)
+    self.bme280 = bme280 = qwiic_bme280.QwiicBme280(address=bme280_address)
+    self.ccs811.begin()
+    self.bme280.begin()
+
+  def reading(self):
+      """
+      Return a hash containing sensor readings.
+      The hash will contain:
+          temp_C
+          press_mb
+          rh
+      """
+      self.read_ccs811()
+
+      retval = {}
+      retval["tvoc_ppb"] = "{:.2f}".format(self.tvoc)
+      retval["eco2_ppm"] = "{:.2f}".format(self.eco2)
+      retval["pres_mb"] = "{:.2f}".format(self.pres)
+      retval["tdry_degc"] = "{:.2f}".format(self.tdry)
+      retval["rh"] = "{:.2f}".format(self.rh)
+      return retval
+
+  def read_bme280(self):
+      self.rh = self.bme280.humidity
+      self.tdry = self.bme280.temperature_celsius
+      self.pres = self.bme280.pressure/100.0
+
+  def read_ccs811(self, read_bme=True):
+
+    if read_bme:
+      self.read_bme280()
+    self.ccs811.set_environmental_data(self.rh, self.tdry)
+    while not self.ccs811.data_available():
+      print("CCS811 starting up")
+      time.sleep(1)
+
+    if self.ccs811.data_available():
+      self.ccs811.read_algorithm_results()
+      self.eco2 = self.ccs811.CO2
+      self.tvoc = self.ccs811.TVOC
+
+    else:
+      print("CCS811  data not avaiable")
+      if self.ccs811.check_status_error():
+        error = self.ccs811.get_error_register();
+        if error == 0xFF:   
+          # communication error
+          print("CCS811 failed to get Error ID register from sensor")
+        else:
+          strErr = "CCS811 unknown error"
+          for code in self._deviceErrors.keys():
+            if error & code:
+              strErr = self._deviceErrors[code]
+              break
+          print("CCS811 device error: %s" % strErr)
+
+
+if __name__ == '__main__':
+    device = ccs811_bme280()
+    while True:
+      print(device.reading())
+      time.sleep(1)
